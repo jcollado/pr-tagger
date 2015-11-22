c664ebdc418d7ec6604fb4adad9de269c374f786
Javier Collado <javier.collado@gmail.com>
2015-11-22 04:28:13 +0100

    Get commit list from repo in current directory
    Using just the basic example in [commit-stream](https://www.npmjs.com/package/commit-stream)

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 #!/usr/bin/env node
+const commitStream = require('commit-stream')
+const listStream = require('list-stream')
 const program = require('commander')
+const split2 = require('split2')
+
+const spawn = require('child_process').spawn
 
 const version = require('./package').version
 
 program
   .version(version)
   .parse(process.argv)
+
+spawn('bash', ['-c', 'git log'])
+  .stdout
+    .pipe(split2())
+    .pipe(commitStream())
+    .pipe(listStream.obj(onCommitList))
+
+function onCommitList (error, commitList) {
+  if (error) throw error
+
+  commitList.forEach(function (commit) {
+    console.log(commit)
+  })
+}
