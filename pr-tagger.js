d3f36eb1a3dc6d58b980215561e73270c0845aff
Javier Collado <javier.collado@gmail.com>
2015-11-26 10:32:06 +0100

    Use winston instead of debug
    Winston provides different log levels

--- pr-tagger.js
+++ pr-tagger.js
@@ -5,17 +5,23 @@ const path = require('path')
 
 const pkgToId = require('pkg-to-id')
 const program = require('commander')
+const winston = require('winston')
 
 
 const pkg = require('./package')
-const debug = require('debug')(pkg.name)
 
 const pkgFile = path.join(process.cwd(), 'package.json')
 const pkgData = fs.existsSync(pkgFile) ? require(pkgFile) : {}
 const pkgId = pkgToId(pkgData)
 
-debug(pkg.name + ' v' + pkg.version)
-debug('pkgId: ' + JSON.stringify(pkgId))
+const logger = new winston.Logger({
+  level: 'debug',
+  transports: [new winston.transports.Console()]
+})
+logger.cli()
+
+logger.info('%s v%s', pkg.name, pkg.version)
+logger.debug('pkgId:', pkgId)
 
 program
   .version(pkg.version)
@@ -23,6 +29,7 @@ program
   .option('-u, --user [user]', 'GitHub [user]', pkgId.user)
   .option('-p, --project [project]', 'GitHub [project]', pkgId.name)
   .parse(process.argv)
+logger.debug('program:', {user: program.user, project: program.project})
 
 
 
