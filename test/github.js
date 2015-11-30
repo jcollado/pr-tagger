c968741de42dc0da6abd4955ce9fd944843aa650
Javier Collado <javier.collado@gmail.com>
2015-11-30 23:30:12 +0100

    Add test case to verify no commits found

--- test/github.js
+++ test/github.js
@@ -1,4 +1,4 @@
-/* global describe it */
+/* global describe it beforeEach */
 'use strict'
 
 const chai = require('chai')
@@ -8,15 +8,27 @@ const sinon = require('sinon')
 const expect = chai.expect
 
 describe('getMergeCommits', function () {
-  it('returns one commit per line', function () {
-    let stubs = {}
-    stubs[require.resolve('../lib/util')] = {
-      exec: sinon.stub().returns(new Buffer('commit 1\ncommit 2\n'))
-    }
+  let stubs
+  let exec
 
+  beforeEach('create stubs', function () {
+    exec = sinon.stub()
+    stubs = {}
+    stubs[require.resolve('../lib/util')] = { exec }
+  })
+
+  it('returns one commit per line', function () {
+    exec.returns(new Buffer('commit 1\ncommit 2\n'))
     const github = requireInject('../lib/github', stubs)
 
     expect(github.getMergeCommits('a..b'))
       .to.deep.equal(['commit 1', 'commit 2'])
+  })
+
+  it('returns empty list when no commits are found', function () {
+    exec.returns(new Buffer(''))
+    const github = requireInject('../lib/github', stubs)
+
+    expect(github.getMergeCommits('a..b')).to.deep.equal([])
   })
 })
