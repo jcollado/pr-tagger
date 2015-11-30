e8dd8c2563ce07766b530e6856ed9f0c1396f9b6
Javier Collado <javier.collado@gmail.com>
2015-11-30 23:39:15 +0100

    Add getPRs test cases

--- test/github.js
+++ test/github.js
@@ -25,10 +25,28 @@ describe('getMergeCommits', function () {
       .to.deep.equal(['commit 1', 'commit 2'])
   })
 
-  it('returns empty list when no commits are found', function () {
+  it('returns empty array when no commits are found', function () {
     exec.returns(new Buffer(''))
     const github = requireInject('../lib/github', stubs)
 
     expect(github.getMergeCommits('a..b')).to.deep.equal([])
+  })
+})
+
+describe('getPRs', function () {
+  const getPRs = require('../lib/github').getPRs
+
+  it('filters commits with PRs', function () {
+    const prs = getPRs([
+      'this is a commit',
+      'Merge pull request #42 from ',
+      'this is another commit'
+    ])
+    expect(prs).to.deep.equal([42])
+  })
+
+  it('return empty array when no commits have been passed', function () {
+    const prs = getPRs([])
+    expect(prs).to.deep.equal([])
   })
 })
