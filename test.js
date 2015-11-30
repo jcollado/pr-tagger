92a68ea449599b87ff3862b3a087f314878ddcb7
Javier Collado <javier.collado@gmail.com>
2015-11-30 14:07:24 +0100

    Add more test cases for the `exec` function

--- test.js
+++ test.js
@@ -33,8 +33,34 @@ describe('exec', function () {
   it('writes the command to the log', function () {
     const prTagger = requireInject('./pr-tagger', stubs)
     const logger = prTagger.logger
+
     prTagger.exec(command)
     expect(logger.debug).to.have.been.calledWith('Command: %s', command)
+  })
+
+  it('returns command stdout on success', function () {
+    const expectedStdout = 'this is the command stdout'
+    stubs.child_process.execSync.returns(expectedStdout)
+    const prTagger = requireInject('./pr-tagger', stubs)
+
+    const stdout = prTagger.exec(command)
+    expect(stdout).to.equal(expectedStdout)
+  })
+
+  it('logs error and exits on failure', function () {
+    const expectedError = new Error('some error')
+    stubs.child_process.execSync.throws(expectedError)
+    const prTagger = requireInject('./pr-tagger', stubs)
+    const logger = prTagger.logger
+
+    sinon.stub(process, 'exit')
+    try {
+      prTagger.exec(command)
+      expect(logger.error).to.have.been.calledWith(expectedError)
+      expect(process.exit).to.have.been.calledWith(1)
+    } finally {
+      process.exit.restore()
+    }
   })
 })
 
