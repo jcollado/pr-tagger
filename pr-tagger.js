25cafc7d49e2b42823a20d7a0c17070767cde7b2
Javier Collado <javier.collado@gmail.com>
2015-12-02 13:53:04 +0100

    Split writeComment into two separate functions

--- pr-tagger.js
+++ pr-tagger.js
@@ -15,40 +15,42 @@ const logger = require('./lib/logging').logger
 const parseArguments = require('./lib/arguments').parseArguments
 const pkg = require('./package')
 
-function writeComments (authOptions, program, prs, comment) {
+function authorize (authOptions, program, prs, comment) {
   return ghauth(authOptions).then(
     function (authData) {
       logger.debug('GitHub Authorization success for user: %s', authData.user)
-
-      return Promise.all(prs.map(function (pr) {
-        logger.info('Checking PR#%d comments...', pr)
-        return ghissues.listComments(authData, program.user, program.project, pr)
-          .then(
-            function (commentList) {
-              const semverComments = github.getSemverComments(commentList)
-
-              if (semverComments.length > 0) {
-                logger.warn(
-                  'Semver comments found in PR#%d: %s',
-                  pr, JSON.stringify(semverComments))
-              } else {
-                logger.info('Adding comment to PR#%d', pr)
-                if (!program.dryRun) {
-                  return github.writeComment(
-                    authData, program.user, program.project, pr, comment)
-                }
-              }
-            },
-            function (error) {
-              logger.error('Error checking PR#%d comments: %s', pr, error)
-            })
-      }))
+      return writeComments(authData, program, prs, comment)
     },
     function (error) {
       logger.error('GitHub Authorization failure: %s', error)
       process.exit(1)
-    }
-  )
+    })
+}
+
+function writeComments (authData, program, prs, comment) {
+  return Promise.all(prs.map(function (pr) {
+    logger.info('Checking PR#%d comments...', pr)
+    return ghissues.listComments(authData, program.user, program.project, pr)
+    .then(
+      function (commentList) {
+        const semverComments = github.getSemverComments(commentList)
+
+        if (semverComments.length > 0) {
+          logger.warn(
+            'Semver comments found in PR#%d: %s',
+            pr, JSON.stringify(semverComments))
+        } else {
+          logger.info('Adding comment to PR#%d', pr)
+          if (!program.dryRun) {
+            return github.writeComment(
+              authData, program.user, program.project, pr, comment)
+          }
+        }
+      },
+      function (error) {
+        logger.error('Error checking PR#%d comments: %s', pr, error)
+      })
+  }))
 }
 
 function main () {
@@ -118,7 +120,7 @@ function main () {
     note: pkg.name,
     scopes: ['repo']
   }
-  writeComments(authOptions, program, prs, toTag).then(
+  authorize(authOptions, program, prs, toTag).then(
     function () {
       logger.info('Done!')
     },
