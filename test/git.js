5f296e85f267e03b74900b31b3237b292c759145
Javier Collado <javier.collado@gmail.com>
2015-12-04 13:25:31 +0100

    Split git and github code in separate modules

---
+++ test/git.js
@@ -0,0 +1,80 @@
+/* global describe it beforeEach */
+'use strict'
+
+const chai = require('chai')
+const requireInject = require('require-inject')
+const sinon = require('sinon')
+
+const expect = chai.expect
+
+describe('getSemverTags', function () {
+  let stubs
+  let exec
+
+  beforeEach('create stubs', function () {
+    exec = sinon.stub()
+    stubs = {}
+    stubs[require.resolve('../lib/util')] = { exec }
+  })
+
+  it('returns semver tags only in an array', function () {
+    exec.returns(new Buffer('v1.0.0\nnot-semver\nv0.2.1\nv0.2.0\nv0.1.0\n'))
+    const git = requireInject('../lib/git', stubs)
+
+    const tags = git.getSemverTags()
+    expect(tags).to.deep.equal(['v1.0.0', 'v0.2.1', 'v0.2.0', 'v0.1.0'])
+  })
+
+  it('returns empty array when no tags found', function () {
+    exec.returns(new Buffer('not-semver\n'))
+    const git = requireInject('../lib/git', stubs)
+
+    const tags = git.getSemverTags()
+    expect(tags).to.deep.equal([])
+  })
+})
+
+describe('getMergeCommits', function () {
+  let stubs
+  let exec
+
+  beforeEach('create stubs', function () {
+    exec = sinon.stub()
+    stubs = {}
+    stubs[require.resolve('../lib/util')] = { exec }
+  })
+
+  it('returns one commit per line', function () {
+    exec.returns(new Buffer('commit 1\ncommit 2\n'))
+    const git = requireInject('../lib/git', stubs)
+
+    expect(git.getMergeCommits('a..b'))
+      .to.deep.equal(['commit 1', 'commit 2'])
+  })
+
+  it('returns empty array when no commits are found', function () {
+    exec.returns(new Buffer(''))
+    const git = requireInject('../lib/git', stubs)
+
+    expect(git.getMergeCommits('a..b')).to.deep.equal([])
+  })
+})
+
+describe('getPRs', function () {
+  const getPRs = require('../lib/git').getPRs
+
+  it('filters commits with PRs', function () {
+    const prs = getPRs([
+      'this is a commit',
+      'Merge pull request #42 from ',
+      'this is another commit'
+    ])
+    expect(prs).to.deep.equal([42])
+  })
+
+  it('return empty array when no commits have been passed', function () {
+    const prs = getPRs([])
+    expect(prs).to.deep.equal([])
+  })
+})
+
