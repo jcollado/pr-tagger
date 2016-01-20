04ae9628f5586bee1278816783315c356cf5a374
Javier Collado <javier.collado@gmail.com>
2016-01-20 13:52:08 +0100

    Add comment with a markdown link to test
    This is the kind of message that pr-tagger should write

--- test/github/util.js
+++ test/github/util.js
@@ -65,14 +65,16 @@ describe('github.util.getSemverComments', function () {
       {body: 'v0.0.0'},
       {body: 'v0.1.0'},
       {body: 'another comment'},
-      {body: 'semver v0.2.0 embedded in a comment'}
+      {body: 'semver v0.2.0 embedded in a comment'},
+      {body: 'Released in: [v0.3.0](https://github.com/user/project/releases/tag/v0.3.0)'}
     ]
 
     expect(util.getSemverComments(commentList))
       .to.deep.equal([
         'v0.0.0',
         'v0.1.0',
-        'semver v0.2.0 embedded in a comment'
+        'semver v0.2.0 embedded in a comment',
+        'Released in: [v0.3.0](https://github.com/user/project/releases/tag/v0.3.0)'
       ])
   })
 
